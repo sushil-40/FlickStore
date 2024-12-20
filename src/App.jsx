031508c29ab0110cb38f8710958bc60d68234600
Sushil <grayson.9999@gmail.com>
2024-12-20 19:57:10 +1000

    Starting Movie Store named FlickStore Application

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+import "./App.css";
+import { MovieCard } from "./components/MovieCard";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <header className="title  font-effect-shadow-multiple">
+        Flick Store
+      </header>
+
+      <div className="hero-container">
+        <div className="hero-content row">
+          <div className="left-container col-12 col-md-4  d-flex justify-content-center align-items-center">
+            <div className="movie-search-container m-2 d-flex flex-column">
+              <div className="movie-search d-flex justify-content-center align-items-start m-2 rounded">
+                <form>
+                  <div className="mb-3">
+                    <p className="desc">Search millions of Movies online!</p>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="enter movie name"
+                    />
+                  </div>
+                  <div className="search-btn-container">
+                    <button
+                      type="submit"
+                      className="btn btn-primary search-btn"
+                    >
+                      Search
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="searched-movie">
+                <MovieCard />
+              </div>
+            </div>
+          </div>
+          <div className="right-container col-12 col-md-8   ">
+            <div className="video-preview-container d-flex justify-content-center align-items-center flex-column">
+              <div className="modern-tv mt-2">
+                <div className="tv-screen">
+                  <video autoPlay loop muted>
+                    <source src="your-video.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+                <div className="tv-stand"></div>
+              </div>
+              <hr className="line" />
+              <div className="trending-movies">
+                <p className="sub-title">Trending Movies</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer className="footer-section  m-2">
+        &copy; 2024 Copyright all reserved || Sushil Dangoriya
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export const MovieCard = () => {
+  return (
+    <div className="container">
+      <div className="col border rounded text-dark p-3 movie-card-item">
+        <div className="col-md">
+          <img src="../src/assets/ab.jpg" alt="" />
+        </div>
+        <div className="col-md">
+          <h3> </h3>
+          <p>IMDB Rating: </p>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
+            iste?...
+          </p>
+          {/* {!mood && ( */}
+          <div className="d-flex justify-content-between gap-2">
+            <button
+              className="btn btn-warning flex-grow-1"
+              // onClick={() => handleOnAddToTheList("drama")}
+            >
+              Drama
+            </button>
+            <button
+              className="btn btn-info flex-grow-1"
+              // onClick={() => handleOnAddToTheList("action")}
+            >
+              Action
+            </button>
+          </div>
+
+          <div className="d-grid mt-3">
+            <button
+              //   onClick={() => deleteFunc(imdbID)}
+              className="btn btn-danger"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
